f5abadc54faa49689cc29dd4178be7c18d18fcb3
Arisa Shinozuka <arisa-shinozuka@hotmail.com>
2021-09-20 19:00:55 +0900

    Foundational Java ch15: JDBC
    I've got a connection! with PostgreSQL. Don't forget to add driver JAR file to library.

--- src/fundamentaljava/ch15.java
+++ src/fundamentaljava/ch15.java
@@ -10,25 +10,27 @@ public class ch15 {
 
     public static void main(String[] args) {
 
-        Connection conn = null;
-        String url = "jdbc:mysql://localhost/courses?autoReconnect=true&useSSL=false";
-        String user = "as";
-        String password = "arisa8585";
-        // for mac
-//        String user = "root";
-//        String password = "";
+        Connection connection = null;
+//        String url = "jdbc:mysql://192.168.3.140/courses?autoReconnect=true&useSSL=false";
+//        String url = "jdbc:mysql://localhost/courses?autoReconnect=true&useSSL=false&serverTimezone=JST";
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+
+//        String user = "as";
+//        String password = "arisa8585";
+        String user = "postgres";
+        String password = "arisa";
 
         try
         {
-            conn = DriverManager.getConnection(url, user, password);
+            connection = DriverManager.getConnection(url, user, password);
 //            Statement statement = connection.createStatement();
             System.out.println("connected");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         } finally{
             try{
-                if (conn != null){
-                    conn.close();
+                if (connection != null){
+                    connection.close();
                 }
             } catch (SQLException sqle){
                 sqle.printStackTrace();
